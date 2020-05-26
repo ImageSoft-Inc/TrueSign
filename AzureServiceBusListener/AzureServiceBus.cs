511e4e6d76d4ef3c5f569b8758d32a1ae65284cb
Julian Vataj <jvataj@imagesoftinc.com>
2020-05-26 11:39:30 -0400

    Started the Azure Service Bus Listener sample service

---
+++ AzureServiceBusListener/AzureServiceBus.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Microsoft.Azure.ServiceBus;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace AzureServiceBusListener
+{
+    public enum ServiceState
+    {
+        SERVICE_STOPPED = 0x00000001,
+        SERVICE_START_PENDING = 0x00000002,
+        SERVICE_STOP_PENDING = 0x00000003,
+        SERVICE_RUNNING = 0x00000004,
+        SERVICE_CONTINUE_PENDING = 0x00000005,
+        SERVICE_PAUSE_PENDING = 0x00000006,
+        SERVICE_PAUSED = 0x00000007,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ServiceStatus
+    {
+        public int dwServiceType;
+        public ServiceState dwCurrentState;
+        public int dwControlsAccepted;
+        public int dwWin32ExitCode;
+        public int dwServiceSpecificExitCode;
+        public int dwCheckPoint;
+        public int dwWaitHint;
+    };
+
+    public partial class AzureServiceBus : ServiceBase
+    {
+        private static IQueueClient queueClient;
+
+        public AzureServiceBus()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Update the service state to Start Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            string serviceBusConnectionString = ConfigurationManager.AppSettings["AzureBusConn"] ?? throw new Exception("Error: Azure Service Bus connection string was not present in the App.config file.");
+
+            var csb = new ServiceBusConnectionStringBuilder(serviceBusConnectionString);
+            queueClient = new QueueClient(csb);
+
+            // Register QueueClient's MessageHandler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override async void OnStop()
+        {
+            // Update the service state to Stop Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOP_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+            
+            // Close the queue client before stopping.
+            await queueClient.CloseAsync();
+
+            // Update the service state to Stopped.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
+
+        void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            dynamic envelope = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(message.Body));
+
+            //TO DO: do something with the received envelope.
+
+
+
+            // Complete the message so that it is not received again.
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        // Use this Handler to look at the exceptions received on the MessagePump
+        Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            this.EventLog.WriteEntry($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.", EventLogEntryType.Error);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AzureServiceBusListener/ProjectInstaller.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AzureServiceBusListener
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AzureServiceBusProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.AzureServiceBusListenerInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // AzureServiceBusProcessInstaller
+            // 
+            this.AzureServiceBusProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.AzureServiceBusProcessInstaller.Password = null;
+            this.AzureServiceBusProcessInstaller.Username = null;
+            // 
+            // AzureServiceBusListenerInstaller
+            // 
+            this.AzureServiceBusListenerInstaller.Description = "A sample service that connects to an Azure Service Bus queue and listens for new " +
+    "messages.";
+            this.AzureServiceBusListenerInstaller.DisplayName = "Azure Service Bus Listener";
+            this.AzureServiceBusListenerInstaller.ServiceName = "AzureServiceBusListener";
+            this.AzureServiceBusListenerInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.AzureServiceBusProcessInstaller,
+            this.AzureServiceBusListenerInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller AzureServiceBusProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller AzureServiceBusListenerInstaller;
+    }
+}
